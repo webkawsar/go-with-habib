77c24b7f65280e261664e832142228a0815944df
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-07-18 12:44:43 +0600

    practice go variable data types

---
+++ class-8/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	// a := 10
+
+	// fmt.Println(a)
+
+	// Variable & Data Types
+	// String, Numeric, Boolean
+	// string, int, float32/float64, bool
+	// varibale declare korle memory te giye space allocate kore
+
+	// variable declaration
+	// var x int = 101
+	// var x = 101
+
+	// shorthand
+	x := 101
+	fmt.Println(x)
+
+	// shorthand e first time variable assign korar somoy : dite hobe
+	// pore re-assign korar somoy : dite hobe na
+	xyz := 420
+	xyz = 247
+	fmt.Println(xyz)
+
+	// onno data type diye re-assign kora jabe na error show korbe
+	xyz = "Something"
+	fmt.Println(xyz)
+}
+
+
+
+
+
+
+
+
+
