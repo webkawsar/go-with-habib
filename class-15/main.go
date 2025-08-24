3f883c377ecdcf7b008dd868ab4b0c6fe1659def
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-08-24 22:37:21 +0600

    practices scope types

---
+++ class-15/main.go
@@ -0,0 +1,29 @@
+
+package main
+import "fmt"
+
+// scope
+// global scope
+// local scope
+// package
+
+
+var p = 10
+var q = 70
+
+func add(num1 int, num2 int) int {
+	sum := num1 + num2
+
+	return sum
+}
+
+func main() {
+	var a = 20
+	var b = 30
+
+	
+	fmt.Println(add(a, b))
+	fmt.Println(add(p, q))
+	// fmt.Println(add(p, sum))
+}
+
