e676cb1e46dd31bc7e332a5d78188056466ff55a
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-09-20 22:47:28 +0600

    practice named function or standard function

---
+++ class-19/main.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+// Function Types
+// Standard Function or Named Function
+
+func add(a int, b int) {
+	fmt.Println(a+b)
+}
+
+func main() {
+	add(10, 11)
+}
+
+
+
+
+
+
