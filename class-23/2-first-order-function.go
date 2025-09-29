8ecdcce84dbdae2176996f7820c987b82a3c0525
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-09-29 14:47:35 +0600

    practice first order function, higher order function, first class citizen

---
+++ class-23/2-first-order-function.go
@@ -0,0 +1,27 @@
+package main
+import "fmt"
+
+// first order function
+// standard function or named function
+// anonymous function
+// IIFE
+// function expression
+
+
+// functional paradigm => haskel, racket
+// math => logic (discrete mathematics)
+// first order logic
+// higher order logic
+
+
+// logic
+// Object
+// Property
+// Relation
+
+
+
+func main() {
+	fmt.Println("Hello World")
+}
+
---
+++ class-23/3-higher-order-function.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+// higher order function
+// je function parameter hisebe arekta function receive korte pare or function return kore sei function ke higher order function bole
+
+// callback function
+// higher order function er maje parameter/argument hisebe je function pass kora hoy seta callback function
+
+// first class function (treated as first class citizen)
+// variable er maje assign korte parake first class citizen bole
+
+
+
+func main() {
+	fmt.Println("Hello World")
+}
+
