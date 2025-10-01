eb9c8d9215a968caae7a0a7b075263cd9a9b5bb9
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-10-01 23:53:18 +0600

    practice go internal memory

---
+++ class-25/main.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+// 2 ta Phase
+// compilation
+// execution
+
+
+// compilation phase
+// sokol constant bose
+// function definition bose
+
+
+
+func main() {
+	fmt.Println("Hello World")
+}
+
