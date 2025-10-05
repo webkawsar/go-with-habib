222118d937623fc83128a78d1d2002d2fb90e36e
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-10-05 23:55:31 +0600

    practice go internal mechanism

--- class-25/main.go
+++ class-25/main.go
@@ -1,6 +1,7 @@
 package main
 import "fmt"
 
+// go er code run hoy 2ta way te
 // 2 ta Phase
 // compilation
 // execution
@@ -11,8 +12,42 @@ import "fmt"
 // function definition bose
 
 
+const a = 10
+var p = 120
+
+func call() {
+	add := func (x int, y int) {
+		z := x + y
+		fmt.Println(z)
+	}
+
+	add(5, 6)
+	add(p, a)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	call()
+
+	fmt.Println(a)
 }
+
+func init() {
+	fmt.Println("Hello from init")
+}
+
+
+/*
+
+	Compilation Phase
+	ei phase e code binary file e convert hoy
+
+	**** Binary File ****
+	go build main.go
+	./main
+
+	go run main.go => compile it => main => ./main
+	go build main.go
+
+*/
+
 
