65a5d0df68b1a67a9f61c4a6cac11fa68482988d
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2025-10-07 23:38:51 +0600

    practice end of internal memory

--- class-25/main.go
+++ class-25/main.go
@@ -38,16 +38,33 @@ func init() {
 
 /*
 
-	Compilation Phase
+	******* Compilation Phase *******
 	ei phase e code binary file e convert hoy
+
+	** Code Segment **
+	a = 10
+	call = func () {....}
+	add = func () {....}
+	main = func () {....}
+	init = func () {....}
+
+
+	******** Execution Phase **********
+
+	
+
 
 	**** Binary File ****
 	go build main.go
 	./main
 
 	go run main.go => compile it => main => ./main
-	go build main.go
+	go build main.go => compile it => main
+	./main
 
 */
 
 
+// compilation phase e check korbe main, init function ache kina
+// compilation phase ei binary file er majer code segment e ache
+// je function ta run hoy tokhon stack er majhe koyekta cell niye seti stack frame hoy
